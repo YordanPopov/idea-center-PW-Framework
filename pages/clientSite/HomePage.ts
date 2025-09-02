f20c21c6ccd5092fbf9d72a80a56226b28ab1053
YordanPopov <jordanpopov190@gmail.com>
2025-09-02 21:34:33 +0300

    add HomePage object page

---
+++ pages/clientSite/HomePage.ts
@@ -0,0 +1,20 @@
+import { Page, Locator, expect } from "@playwright/test";
+/**
+ * This is the page object for the HomePage Page.
+ * @export
+ * @class HomePage
+ * @typedef {HomePage}
+ */
+export class HomePage {
+  constructor(private page: Page) {}
+
+  get homePageCarousel(): Locator {
+    return this.page.locator(".carousel");
+  }
+
+  async navigateToHomePage(): Promise<void> {
+    await this.page.goto(process.env.URL as string);
+
+    expect(this.homePageCarousel).toBeVisible();
+  }
+}
