dd1436b7e19ca57853362546c75e93e5cfcafc09
YordanPopov <jordanpopov190@gmail.com>
2025-09-05 22:22:55 +0300

    refactor: rename getter for idea description and refactor delete idea method

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,7 +3,7 @@ import { test as setup } from "../fixtures/pom/test-options";
 setup("auth user", async ({ homePage, navPage, loginPage, page }) => {
   await setup.step("create logged in user session", async () => {
     await homePage.navigateToHomePage();
-    await navPage.openLoginPage(loginPage);
+    await navPage.openLoginPage();
     await loginPage.login(process.env.EMAIL!, process.env.PASSWORD!);
 
     await page.context().storageState({ path: ".auth/userSession.json" });
