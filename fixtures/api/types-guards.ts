c730cbbaca26bdb88088fda65468872c4a7959c1
YordanPopov <jordanpopov190@gmail.com>
2025-09-13 22:21:47 +0300

    add types

---
+++ fixtures/api/types-guards.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import type {
+  CreateIdeaResponseSchema,
+  UserSchema,
+  ErrorResponseSchema,
+  IdeaResponseSchema,
+} from "./schemas";
+
+export type ApiRequestParams = {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  url: string;
+  baseUrl?: string;
+  body?: Record<string, unknown> | null;
+  headers?: string;
+};
+
+export type ApiRequestResponse<T = unknown> = {
+  status: number;
+  body: T;
+};
+
+export type ApiRequestFn = <T = unknown>(
+  params: ApiRequestParams
+) => Promise<ApiRequestResponse<T>>;
+
+export type ApiRequestMethods = {
+  apiRequest: ApiRequestFn;
+};
+
+export type User = z.infer<typeof UserSchema>;
+export type ErrorResponse = z.infer<typeof ErrorResponseSchema>;
+export type CreateIdeaResponse = z.infer<typeof CreateIdeaResponseSchema>;
+export type IdeaResponse = z.infer<typeof IdeaResponseSchema>;
