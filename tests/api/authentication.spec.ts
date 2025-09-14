c500dd76b6447763d76210e5726af5bebd7328c4
YordanPopov <jordanpopov190@gmail.com>
2025-09-14 21:00:10 +0300

    add API validation tests

--- tests/api/authentication.spec.ts
+++ tests/api/authentication.spec.ts
@@ -22,4 +22,69 @@ test.describe("Verify API Validation for Log In / Sign Up", () => {
       expect(ErrorResponseSchema.parse(body)).toBeTruthy();
     }
   );
+
+  test(
+    "Verify API Validation for Sign Up",
+    { tag: "@Api" },
+    async ({ apiRequest }) => {
+      await test.step("Verify API Validation for Invalid Email", async () => {
+        for (const invalidEmail of invalidCredentials.invalidEmails) {
+          const { status, body } = await apiRequest<ErrorResponse>({
+            method: "POST",
+            url: "User/Create",
+            baseUrl: process.env.API_URL,
+            body: {
+              userName: `testuser${Date.now()}`,
+              email: invalidEmail,
+              password: "test1234",
+              rePassword: "test1234",
+              acceptedAgreement: true,
+            },
+          });
+
+          expect(status).toBe(400);
+          expect(ErrorResponseSchema.parse(body)).toBeTruthy();
+        }
+      });
+
+      await test.step("Verify API Validation for Invalid Username", async () => {
+        for (const invalidUsername of invalidCredentials.invalidUsernames) {
+          const { status, body } = await apiRequest<ErrorResponse>({
+            method: "POST",
+            url: "User/Create",
+            baseUrl: process.env.API_URL,
+            body: {
+              userName: invalidUsername,
+              email: `testuser${Date.now()}@email.com`,
+              password: "test1234",
+              rePassword: "test1234",
+              acceptedAgreement: true,
+            },
+          });
+
+          expect(status).toBe(400);
+          expect(ErrorResponseSchema.parse(body)).toBeTruthy();
+        }
+      });
+      await test.step("Verify API Validation for Invalid Password", async () => {
+        for (const invalidPassword of invalidCredentials.invalidPasswords) {
+          const { status, body } = await apiRequest<ErrorResponse>({
+            method: "POST",
+            url: "User/Create",
+            baseUrl: process.env.API_URL,
+            body: {
+              userName: `testuser${Date.now()}`,
+              email: `testuser${Date.now()}@email.com`,
+              password: invalidPassword,
+              rePassword: invalidPassword,
+              acceptedAgreement: true,
+            },
+          });
+
+          expect(status).toBe(400);
+          expect(ErrorResponseSchema.parse(body)).toBeTruthy();
+        }
+      });
+    }
+  );
 });
