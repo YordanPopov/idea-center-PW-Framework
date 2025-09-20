ac08880c01cfe721ef1da0dc31f82b1481ad3db4
YordanPopov <jordanpopov190@gmail.com>
2025-09-20 17:55:32 +0300

    add failing test

---
+++ tests/clientSite/failingTest.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from "../../fixtures/pom/test-options";
+
+test("Failing API test", { tag: "@Api" }, async ({ homePage }) => {
+  await homePage.navigateToHomePage();
+  expect(2).toBe(3);
+});
