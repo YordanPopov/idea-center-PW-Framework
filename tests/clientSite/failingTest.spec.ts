0413fe7a56bb4ceef31ffe69523191c616494f7c
YordanPopov <jordanpopov190@gmail.com>
2025-09-20 17:58:15 +0300

    just commit for test

--- tests/clientSite/failingTest.spec.ts
+++ tests/clientSite/failingTest.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "../../fixtures/pom/test-options";
 
 test("Failing API test", { tag: "@Api" }, async ({ homePage }) => {
   await homePage.navigateToHomePage();
-  expect(2).toBe(3);
+  expect(2).toBe(4);
 });
