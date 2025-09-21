75fdbabc9173c21e54daeee43cdc30d4acaf6319
YordanPopov <jordanpopov190@gmail.com>
2025-09-21 14:01:06 +0300

    add failing test for demonstration purpose

---
+++ tests/clientSite/failingTests.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "../../fixtures/pom/test-options";
+
+test("Failing API test", { tag: "@Api" }, async ({ homePage }) => {
+  await homePage.navigateToHomePage();
+
+  expect(2).toBe(3);
+});
+
+test("Failing Smoke test", { tag: "@Smoke" }, async ({ homePage }) => {
+  await homePage.navigateToHomePage();
+
+  expect(2).toBe(3);
+});
